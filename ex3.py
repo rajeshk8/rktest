b2f6a11bf437961147fe57be7a5026a0607ef47b
rk <rajeshk8@yahoo.com>
2017-07-11 14:15:39 -0700

    add ex

---
+++ ex3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+
+
+var_1 = "raj"
+var_2 = "mukesh"
+var_3 =  "shyam"
+var_4 = raw_input("Enter name: ")
+
+print()
+print("{:<30}".format(var_1))
+print("{:<30}".format(var_2))
+print("{:<30}".format(var_3))
+print("{:<30}".format(var_4))
+print()
